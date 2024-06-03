a834096ec6b463f57280e0f97f4d023415ec5337
OonoYouji <k023g0007@g.neec.ac.jp>
2024-06-03 11:14:13 +0900

    マウスボタンを押した場所を指すように画像を回転させるスクリプトを作成

--- Assets/Scripts/ArrowDraw.cs
+++ Assets/Scripts/ArrowDraw.cs
@@ -1,15 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.UI;
+
+
 
 public class ArrowDraw : MonoBehaviour {
+
+	[SerializeField]
+	private Image arrowImage;
+	private Vector3 clickPosition;
+
 	// Start is called before the first frame update
 	void Start() {
-
+		arrowImage.gameObject.SetActive(false);
 	}
 
 	// Update is called once per frame
 	void Update() {
+
+		///- クリックしたタイミングの座標を取得
+		if (Input.GetMouseButtonDown(0)) {
+			clickPosition = Input.mousePosition;
+			arrowImage.gameObject.SetActive(true);
+		}
+
+		///- 押している間矢印の画像を回転させる
+		if (Input.GetMouseButton(0)) {
+			Vector3 dist = clickPosition - Input.mousePosition;
+
+			//- ベクトルの長さを抽出
+			float size = dist.magnitude;
+
+			//- ベクトルから角度を算出
+			float angleRad = Mathf.Atan2(dist.y, dist.x);
+
+			//- 矢印の画像をクリックした場所に移動
+			arrowImage.rectTransform.position = clickPosition;
+			//- 矢印の画像をベクトルから算出した角度を度数方に変換してZ軸回転
+			arrowImage.rectTransform.rotation =
+				Quaternion.Euler(0, 0, angleRad * Mathf.Rad2Deg);
+
+			//- 矢印の画像の大きさをドラッグした距離に合わせる
+			arrowImage.rectTransform.sizeDelta = new Vector2(size, size);
+
+		}
+
+
+		///- 離したら表示しないようにする
+		if (Input.GetMouseButtonUp(0)) {
+			arrowImage.gameObject.SetActive(false);
+		}
+
 
 	}
 }
